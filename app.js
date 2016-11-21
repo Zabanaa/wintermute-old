fcd9e601d5d7b736a4b0fa2b59652e003836720d
= <=>
2016-11-21 09:20:03 +0000

    add dependencies and setup app

---
+++ app.js
@@ -0,0 +1,16 @@
+// Imports
+const express       = require('express')
+const bodyParser    = require('body-parser')
+const app           = express()
+const port          = process.env.PORT || 3000
+
+// Middleware
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+// Routes
+app.get('/', (req, res) => res.send("hello world"))
+app.get('/contact', (req, res) => res.send("contact me"))
+
+// Start app
+app.listen(port, () => console.log(`App started. Server listening on port ${port}`))
