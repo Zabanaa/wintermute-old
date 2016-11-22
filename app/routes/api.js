00da517dd010aac066533784f5f0a1e93337db6c
= <=>
2016-11-22 14:40:10 +0000

    remove /contact endpoint

--- app/routes/api.js
+++ app/routes/api.js
@@ -2,6 +2,5 @@ const express = require('express')
 const router  = express.Router()
 
 router.get('/', (req, res) => res.send("Welcome to the api"))
-router.get("/collection", (req, res) => res.send("another endpoint"))
 
 module.exports = router
