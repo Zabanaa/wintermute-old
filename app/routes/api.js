21eb86944084a72ee25534ba16afcccf13f6d77b
= <=>
2016-11-22 15:57:39 +0000

    create /api/characters endpoint x controller

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,6 +1,16 @@
-const express = require('express')
-const router  = express.Router()
+const express       = require('express')
+const Character     = require('../models/character')
+const router        = express.Router()
 
 router.get('/', (req, res) => res.send("Welcome to the api"))
+
+// GET /api/characters
+router.get('/characters', (req, res) => {
+    Character.find({}, (err, characters) => {
+        if (err) throw err
+        res.status(200)
+        res.json(characters)
+    })
+})
 
 module.exports = router
