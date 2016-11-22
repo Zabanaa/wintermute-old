895afb8ced0c1cfd104b4bb3273c289b54d76df1
= <=>
2016-11-22 10:01:45 +0000

    import api routes to the main app

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express       = require('express')
 const bodyParser    = require('body-parser')
 const app           = express()
 const port          = process.env.PORT || 3000
+const api           = require('./app/routes/api')
 
 // Middleware
 app.use(bodyParser.urlencoded({extended: true}))
@@ -12,6 +13,10 @@ app.use(bodyParser.json())
 // Routes
 app.get('/', (req, res) => res.send("hello world"))
 app.get('/contact', (req, res) => res.send("contact me"))
+
+// api routes
+app.use('/api', api)
+
 
 // Start app
 app.listen(port, () => console.log(`App started. Server listening on port ${port}`))
