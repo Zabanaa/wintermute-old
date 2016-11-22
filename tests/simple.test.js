1f25b89998335ea7291463df9366d4b38d175984
= <=>
2016-11-22 14:40:22 +0000

    create sample test

--- tests/simple.test.js
+++ tests/simple.test.js
@@ -1,9 +1,21 @@
-describe("Array", function(){
+const chai       = require('chai')
+const chaiHttp   = require('chai-http')
+const app        = require('../app')
+const assert     = chai.assert
 
-    it("should blablajaew", function(){
+chai.use(chaiHttp)
+const request = chai.request(app)
 
-        console.log("eheqio")
+describe("Main endpoint work", function(){
 
+    it("should return wass goin on cuzz", function(done){
+
+        request.get('/')
+            .end((err, res) => {
+                 assert.isFalse(res.error)
+                 assert.equal(res.statusCode, 200)
+                 assert.equal(res.text, 'Wass goin on cuzz')
+                 done()
+            })
     })
 })
-
