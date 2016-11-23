a09ed1ca7bb2c4bd763fcb532ced5e4507aeedac
= <=>
2016-11-23 16:18:56 +0000

    add href attribute

--- app/models/character.js
+++ app/models/character.js
@@ -4,14 +4,15 @@ const Schema    = mongoose.Schema
 // Define the character schema
 const character = new Schema({
     name: {type: String, required: true, unique: true},
-    age: Number,
+    age: String,
     birthPlace: String,
     bio: String,
     occupation: String,
-    novel: {type: String, required: true}
+    novel: {type: String, required: true},
+    href: {}
 })
 
 // Create a model out of the schema
-const Character  = mongoose.model('Character', character)
+const Character     = mongoose.model('Character', character)
 
-module.exports = Character
+module.exports      = Character
