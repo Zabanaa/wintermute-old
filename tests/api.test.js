58b3079612f3a5f3b3a10f0dfcf4792c4a1f5df7
= <=>
2016-11-23 09:13:55 +0000

    create tests for GET /api/characters/:id

--- tests/api.test.js
+++ tests/api.test.js
@@ -5,7 +5,7 @@ const assert    = chai.assert
 
 chai.use(chaiHttp)
 
-describe("Test /api/characters", () => {
+describe("Test GET /api/characters", () => {
 
     let request = chai.request(app)
 
@@ -16,11 +16,30 @@ describe("Test /api/characters", () => {
 
                assert.isFalse(res.error)
                assert.equal(res.statusCode, 200)
-               console.log(res)
                assert.isArray(res.body)
                assert.isObject(res.body[0])
                assert.property(res.body[0], 'name')
                done()
+            })
+    })
+})
+
+describe("Test GET /api/characters/:id", () => {
+
+    let request     = chai.request(app)
+    let characterId = "583460b6f0d1cd1d76f437d0"
+
+    it("returns the corresponding character based on the id", (done) => {
+
+        request.get(`/api/characters/${characterId}`)
+            .end( (err, res) => {
+
+                assert.isFalse(res.error)
+                assert.equal(res.statusCode, 200)
+                assert.isObject(res.body)
+                assert.property(res.body, "_id")
+                assert.equal(res.body._id, characterId)
+                done()
             })
     })
 })
