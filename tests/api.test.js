c2779ae89c18a28f1c4b5498bc74baed1efaf018
= <=>
2016-11-24 11:21:53 +0000

    add mongoose and refactor POST /api/characters tests

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,10 +1,22 @@
+const mongoose  = require('mongoose')
+const app       = require('../app')
+const config    = require('../config/db')
+const Character = require('../app/models/character')
 const chai      = require('chai')
 const chaiHttp  = require('chai-http')
-const app       = require('../app')
 const assert    = chai.assert
 
+// connect to test db
+mongoose.createConnection(config.test_db_url)
+
 chai.use(chaiHttp)
+
 let request = chai.request(app)
+
+// Remove Molly from the DB
+after( () => {
+    Character.remove({name: "Molly Millions"}, (err) => {if (err) console.log(err)})
+})
 
 describe("Test GET /api/characters", () => {
 
@@ -12,7 +24,6 @@ describe("Test GET /api/characters", () => {
 
         request.get('/api/characters')
             .end( (err, res) => {
-
                assert.isFalse(res.error)
                assert.equal(res.statusCode, 200)
                assert.isArray(res.body)
@@ -25,7 +36,7 @@ describe("Test GET /api/characters", () => {
 
 describe("Test GET /api/characters/:id", () => {
 
-    let characterId = "583460b6f0d1cd1d76f437d0"
+    let characterId = "5836c030dc84b2e4a2663b78"
 
     it("returns the corresponding character based on the id", (done) => {
 
@@ -43,26 +54,62 @@ describe("Test GET /api/characters/:id", () => {
 
 describe("Test POST /api/characters/", () => {
 
+    let character = {
+        name: "Molly Millions",
+        novel: "Neuromancer"
+    }
+
     it("successfully saves the resource to the DB", (done) => {
 
-        request.post('/api/characters')
-            .end( (err, res) => {
-                assert.isFalse(res.error)
-                assert.equal(res.statusCode, 201)
-                done()
-            })
-    })
-
-    it("Gracefully deals with the validation errors", (done) => {
+        // Make sure the response returns a 201
+        // Make sure That the non required fields return Unknown if not provided
+        // Make sure That the location header is equal to /api/characters/id
 
         request.post('/api/characters')
-            .end( (err, res) => {
-                assert.isFalse(res.error)
-                assert.equal(res.statusCode, 201)
-                done()
-            })
+               .send(character)
+               .end( (err, res) => {
+                   assert.isTrue(res.ok)
+                   assert.isFalse(res.error)
+                   assert.equal(res.statusCode, 201)
+                   assert.equal(res.body.name, character.name)
+                   assert.equal(res.body.bio, "Unknown")
+                   assert.equal(res.body.age, "Unknown")
+                   assert.equal(res.body.occupation, "Unknown")
+                   assert.equal(res.body.birthPlace, "Unknown")
+                   assert.include(res.headers.location, `/api/characters/${res.body._id}`)
+                   done()
+                })
     })
 
+    it("Returns a 422 when passed an already existing character", (done) => {
+
+        request.post('/api/characters')
+                .send(character)
+                .end( (err, res) => {
+                    assert.equal(res.statusCode, 422)
+                    assert.equal(res.body.status, 422)
+                    assert.equal(res.body.error, "A Character with this name already exists")
+                    done()
+                })
+    })
+
+    it("Returns a 422 when missing one or more required fields", (done) => {
+
+        character = {
+            bio: "Badass techno-samurai",
+            occupation: "Mercernary"
+        }
+
+        request.post('/api/characters')
+                .send(character)
+                .end( (err, res) => {
+                    assert.equal(res.statusCode, 422)
+                    assert.equal(res.body.status, 422)
+                    assert.equal(res.body.error, "Missing required fields")
+                    assert.property(res.body, "fields")
+                    assert.isArray(res.body.fields)
+                    assert.include(res.body.fields, "novel", "name")
+                    done()
+                })
+    })
 })
-
-
