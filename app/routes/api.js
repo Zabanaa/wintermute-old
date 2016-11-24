1d56f005e2c1a4b0f67512efcd705d473a9619a5
= <=>
2016-11-24 11:21:13 +0000

    refactor POST /api/character controller to call utils.handleError

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,7 +1,7 @@
 const express       = require('express')
 const Character     = require('../models/character')
 const router        = express.Router()
-const app           = require('../../app')
+const utils         = require('./utils')
 
 const serialise     = (obj, url) => {
     return obj['href'] = url
@@ -34,39 +34,20 @@ router.get('/characters/:id', (req, res) => {
 // after the resource is successfully saved, we alter the object and return it to the user
 router.post('/characters', (req, res) => {
 
-    let data = {
+    character = new Character({
         name: req.body.name,
         age: req.body.age || "Unknown",
         birthPlace: req.body.birthPlace || "Unknown",
         bio: req.body.bio || "Unknown",
         occupation: req.body.occupation || "Unknown",
         novel: req.body.novel
-    }
-    character = new Character(data)
+    })
+
     character.href = `${req.originalUrl}/${character._id}`
 
     character.save((err) => {
-    // if error return handleError(err)
-    if (err) {
 
-        if (err.code === 11000) { // Duplicate Key Constraint
-
-            let fieldName   = err.message.split(".$")[1]
-            fieldName       = fieldName.split(" dup key")[0]
-            fieldName       = fieldName.substring(0, fieldName.lastIndexOf("_"))
-            errorMsg        = `A Character with this ${fieldName} already exists`
-            return res.status(422).json({status: 422, error: errorMsg })
-        }
-
-        else if (err.message.includes(" validation failed")) { // Missing fields
-            let missingFields = Object.keys(err.errors)
-            return res.status(422).json({ status: 422, error: "Missing required fields", fields: missingFields })
-        }
-
-        else {
-            return res.status(500).json({status: 500, error: "An error occured"})
-        }
-    }
+    if (err) return utils.handleError(err, res)
 
     return res.location(`${req.protocol}://${req.get('host')}${character.href}`)
                .status(201)
