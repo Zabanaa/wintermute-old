0d47599eb9422f91da7f30c7c6c65ef02ca949b8
= <=>
2016-11-25 17:00:26 +0000

    refactor PUT /api/characters/id controller to check if objects are identical

--- app/routes/api.js
+++ app/routes/api.js
@@ -54,15 +54,25 @@ router.put('/characters/:id', (req, res) => {
 
     Character.findById(req.params.id, (err, character) => {
 
-        if(!character) {
-            utils.notFound(res)
-        }
+        if(!character) utils.notFound(res) // 404: Bitch Where ?
+
         else {
-            // if req.body.keys === character.keys
-            let updatedChar = utils.updateDoc(character, req.body)
-            character = updatedChar
-            character.save()
-            return res.status(200).json({status: 200, message: "Character successfully updated", character: character})
+
+            if ( utils.assertIdenticalObjects(character._doc, req.body) ) {
+                // The payload object keys match the character object keys
+                // errthang fine -> Return a 200 OK
+                character = utils.updateDoc(character, req.body)
+                character.save()
+                return res.status(200).json({status: 200, message: "Character successfully updated", character: character})
+            }
+            else {
+                // The user did not provide all the keys in the payload. PUT request failed.
+                res.status(400)
+                   .json({
+                       status: 400,
+                       error: "Bad request. Please provide all the fields (even those you do not wish to update)"
+                   })
+            }
         }
     })
 })
