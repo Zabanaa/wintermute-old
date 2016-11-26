8171403f5b67f8e16218355607e4df1c16493060
= <=>
2016-11-26 14:59:26 +0000

    test character.isIdenticalTo and character.update

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,21 +1,51 @@
 const chai       = require('chai')
 const chaiHttp   = require('chai-http')
-const app        = require('../app')
 const assert     = chai.assert
+const Character  = require('../app/models/character')
 
-chai.use(chaiHttp)
-const request = chai.request(app)
+let benz  = new Character({
+    name: "Karim Benz",
+    novel: "Real Madrid CF",
+    age: "9",
+    bio: "hello",
+    occupation: "Baller",
+    birthPlace: "Lyon"
+})
 
-describe("Main endpoint work", function(){
+let nanas = {
+    name: "Samir Nasri",
+    novel: "Sevilla FC S.A.D",
+    age:"22",
+    bio: "Hello",
+    occupation:"player",
+    birthPlace: "Marseille"
+}
 
-    it("should return wass goin on cuzz", function(done){
+describe("Test Character.isIdenticalTo", () => {
 
-        request.get('/')
-            .end((err, res) => {
-                 assert.isFalse(res.error)
-                 assert.equal(res.statusCode, 200)
-                 assert.equal(res.text, 'Wass goin on cuzz')
-                 done()
-            })
+    it("should return true if nanas' keys match benz's keys", () => {
+
+        let identicalObjects = benz.isIdenticalTo(nanas)
+        assert.isTrue(identicalObjects)
+
+    })
+
+    it("should return false if nanas' keys don't match benz's keys", () => {
+
+        delete nanas['age']
+        let identicalObjects = benz.isIdenticalTo(nanas)
+        assert.isFalse(identicalObjects)
+    })
+
+})
+
+describe("Test Character.update", () => {
+
+    it("should update the instance's fields", () => {
+
+        let zizou = {name: "Zinedine Zidane"}
+        benz.update(zizou)
+        assert.equal(benz.name, "Zinedine Zidane")
     })
 })
+
