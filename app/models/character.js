a3c7a73c2bb2420905c71fa32afb146a700b1461
= <=>
2016-11-26 14:59:50 +0000

    add isIdenticalTo and update methods

--- app/models/character.js
+++ app/models/character.js
@@ -12,6 +12,38 @@ const character = new Schema({
     href: {}
 })
 
+/*
+ *   character.isIdenticalTo
+ *
+ *   Compares a request.body object against an instance of Character
+ *   returns true if the keys match otherwise returns false
+ *   Will be used for validation in PUT requests where the client must send
+ *   all the keys for the update to go through
+ *
+*/
+
+character.methods.isIdenticalTo = function(requestBody) {
+
+    if(!requestBody && typeof(requestBody) !== "object") {
+        throw new Error("Function must be passed an object")
+    }
+    let characterKeys       = Object.keys(this._doc).filter( key => key !== "_id" && key !== "__v" && key !== "href").sort()
+    let reqBodyKeys         = Object.keys(requestBody).sort()
+    return JSON.stringify(characterKeys) === JSON.stringify(reqBodyKeys)
+}
+
+/*
+ *   character.update
+ *
+ *   Accepts a request body object and merges it with the current instance
+ *   to update it
+ *
+*/
+
+character.methods.update = function(requestBody) {
+    return Object.assign(this._doc, requestBody)
+}
+
 // Create a model out of the schema
 const Character     = mongoose.model('Character', character)
 
