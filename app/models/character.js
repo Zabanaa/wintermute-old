a6b74afe21f16ec003c8730e1dab94adc8259d6d
= <=>
2016-11-27 19:50:06 +0000

    make name field unique

--- app/models/character.js
+++ app/models/character.js
@@ -2,7 +2,7 @@ const db        = require('./index')
 const mongoose  = require('mongoose')
 const Schema    = mongoose.Schema
 
-let name        = {type: db.Sequelize.STRING, allowNull: false}         // required
+let name        = {type: db.Sequelize.STRING, allowNull: false, unique: true}         // required
 let novel       = {type: db.Sequelize.STRING, allowNull: false}         // required
 let age         = {type: db.Sequelize.STRING, defaultValue: "Unknown"}
 let bio         = {type: db.Sequelize.STRING, defaultValue: "Unknown"}
