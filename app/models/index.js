91a477a6271b993ab923471d3caf4b9ec0f3cf59
= <=>
2016-11-28 10:15:40 +0000

    disable annoying logging of sql queries

--- app/models/index.js
+++ app/models/index.js
@@ -4,12 +4,13 @@ const dbUri             = process.env.DATABASE_URI
 let connection
 
 if (dbUri) {
-    connection    = new Sequelize(dbUri)
+    connection    = new Sequelize(dbUri, {logging: false})
 }
 else {
     connection    = new Sequelize(config.dbname, config.dbuser, config.dbpassword, {
         host: config.dbhost,
-        dialect: config.dbengine
+        dialect: config.dbengine,
+        logging: false
     })
 }
 
