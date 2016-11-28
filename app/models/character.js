72881994cb8a0956df6732805adac6dc5fc58cb7
= <=>
2016-11-28 10:25:23 +0000

    remove mongoose imports and create Character.isIdenticalTo method

--- app/models/character.js
+++ app/models/character.js
@@ -1,14 +1,43 @@
-const db        = require('./index')
-const mongoose  = require('mongoose')
-const Schema    = mongoose.Schema
+const db                = require('./index')
 
-let name        = {type: db.Sequelize.STRING, allowNull: false, unique: true}         // required
-let novel       = {type: db.Sequelize.STRING, allowNull: false}         // required
-let age         = {type: db.Sequelize.STRING, defaultValue: "Unknown"}
-let bio         = {type: db.Sequelize.STRING, defaultValue: "Unknown"}
-let birthPlace  = {type: db.Sequelize.STRING, defaultValue: "Unknown"}
-let occupation  = {type: db.Sequelize.STRING, defaultValue: "Unknown"}
+// Define the columns
+let name                = {type: db.Sequelize.STRING, allowNull: false, unique: true}
+let novel               = {type: db.Sequelize.STRING, allowNull: false}
+let age                 = {type: db.Sequelize.STRING, defaultValue: "Unknown"}
+let bio                 = {type: db.Sequelize.STRING, defaultValue: "Unknown"}
+let birthPlace          = {type: db.Sequelize.STRING, defaultValue: "Unknown"}
+let occupation          = {type: db.Sequelize.STRING, defaultValue: "Unknown"}
 
-const Character = db.connection.define('character', {name, novel, age, bio, birthPlace, occupation})
+let modelAttributes     = {name, novel, age, bio, birthPlace, occupation}
+
+
+/*   character.isIdenticalTo
+ *
+ *   Compares a request.body object against an instance of Character
+ *   returns true if the keys match otherwise returns false
+ *   Will be used for validation in PUT requests where the client must send
+ *   all the keys for the update to go through
+ *
+*/
+
+const isIdenticalTo     = function(requestBody) {
+
+    if (!requestBody && typeof(requestBody) !== "object") throw new Error("Please provide an object")
+
+    let characterKeys   = Object.keys(this.dataValues)
+        .filter(key => key !== 'id' && key !== 'createdAt' && key !== 'updatedAt')
+        .sort()
+
+    // Ignore key's that aren't part of the model
+    // Example: User passes a "playername" key
+    // It's not part of the model so we ignore it
+    let reqBodyKeys     = Object.keys(requestBody)
+        .filter(key => characterKeys.includes(key))
+        .sort()
+
+    return JSON.stringify(characterKeys) === JSON.stringify(reqBodyKeys)
+}
+
+const Character         = db.connection.define('character', modelAttributes, { instanceMethods: {isIdenticalTo} })
 
 module.exports      = Character
