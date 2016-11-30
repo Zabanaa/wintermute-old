7087f2a27bc4911cdd244e83b7d695ecef7c321a
= <=>
2016-11-30 15:32:33 +0000

    remove instance methods and call them from the module

--- app/models/character.js
+++ app/models/character.js
@@ -1,4 +1,5 @@
-const db                = require('../../config')
+const db                                = require('../../config')
+let {serialise, isIdenticalTo}          = require('./methods')
 
 // Define the columns
 let name                = {type: db.Sequelize.STRING, allowNull: false, unique: true}
@@ -10,42 +11,7 @@ let occupation          = {type: db.Sequelize.STRING, defaultValue: "Unknown"}
 
 let modelAttributes     = {name, novel, age, bio, birthPlace, occupation}
 
-const serialise         = function(protocol, host, resourceURI) {
-
-    // alter the instance
-    // add a href url equal to protocol + host + /api/characters/:id
-    return this.dataValues.href = `${protocol}://${host}${resourceURI}`
-}
-
-
-
-/*   character.isIdenticalTo
- *
- *   Compares a request.body object against an instance of Character
- *   returns true if the keys match otherwise returns false
- *   Will be used for validation in PUT requests where the client must send
- *   all the keys for the update to go through
- *
-*/
-
-const isIdenticalTo     = function(requestBody) {
-
-    if (!requestBody && typeof(requestBody) !== "object") throw new Error("Please provide an object")
-
-    let characterKeys   = Object.keys(this.dataValues)
-        .filter(key => key !== 'id' && key !== 'createdAt' && key !== 'updatedAt')
-        .sort()
-
-    // Ignore key's that aren't part of the model
-    // Example: User passes a "playername" key
-    // It's not part of the model so we ignore it
-    let reqBodyKeys     = Object.keys(requestBody)
-        .filter(key => characterKeys.includes(key))
-        .sort()
-
-    return JSON.stringify(characterKeys) === JSON.stringify(reqBodyKeys)
-}
-
-const Character     = db.connection.define('character', modelAttributes, { instanceMethods: {isIdenticalTo, serialise} })
+const Character     = db.connection.define('character', modelAttributes, { instanceMethods: { serialise, isIdenticalTo } })
 
 module.exports      = Character
+
