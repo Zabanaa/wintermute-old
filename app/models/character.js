5371fa27e66046c7e5c18eda8eb6c7f23b42b5da
= <=>
2016-12-01 12:47:19 +0000

    remove Character.novel attribute

--- app/models/character.js
+++ app/models/character.js
@@ -3,15 +3,13 @@ let {serialise, isIdenticalTo}          = require('./methods')
 
 // Define the columns
 let name                = {type: db.Sequelize.STRING, allowNull: false, unique: true}
-let novel               = {type: db.Sequelize.STRING, allowNull: false}
 let age                 = {type: db.Sequelize.STRING, defaultValue: "Unknown"}
 let bio                 = {type: db.Sequelize.STRING, defaultValue: "Unknown"}
 let birthPlace          = {type: db.Sequelize.STRING, defaultValue: "Unknown"}
 let occupation          = {type: db.Sequelize.STRING, defaultValue: "Unknown"}
 
-let modelAttributes     = {name, novel, age, bio, birthPlace, occupation}
+let modelAttributes     = {name, age, bio, birthPlace, occupation}
 
 const Character     = db.connection.define('character', modelAttributes, { instanceMethods: { serialise, isIdenticalTo } })
 
 module.exports      = Character
-
