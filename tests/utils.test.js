a8fd23bc6b82464d4dec5a632711156cfaf41a3b
= <=>
2016-12-02 08:59:30 +0000

    test model methods

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,51 +1,32 @@
-// const chai       = require('chai')
-// const chaiHttp   = require('chai-http')
-// const assert     = chai.assert
-// const Character  = require('../app/models/character')
+const chai       = require('chai')
+const chaiHttp   = require('chai-http')
+const assert     = chai.assert
+const methods    = require('../app/models/methods')
 
-// let benz  = new Character({
-//     name: "Karim Benz",
-//     novel: "Real Madrid CF",
-//     age: "9",
-//     bio: "hello",
-//     occupation: "Baller",
-//     birthPlace: "Lyon"
-// })
+let benz                = {}
+benz.dataValues         = {id: 2, name: "Karim Benzema", club: "Real Madrid CF", position:"CF"}
 
-// let nanas = {
-//     name: "Samir Nasri",
-//     novel: "Sevilla FC S.A.D",
-//     age:"22",
-//     bio: "Hello",
-//     occupation:"player",
-//     birthPlace: "Marseille"
-// }
+let nanas               = {id: 3, name: "Samir Nasri", club: "Sevilla FC", position:"CM"}
 
-// describe("Test Character.isIdenticalTo", () => {
+benz.isIdenticalTo      = methods.isIdenticalTo
+benz.serialise          = methods.serialise
 
-//     it("should return true if nanas' keys match benz's keys", () => {
+describe("Test methods.isIdenticalTo", () => {
 
-//         let identicalObjects = benz.isIdenticalTo(nanas)
-//         assert.isTrue(identicalObjects)
+    it("should return true if nanas' keys match benz's keys", () => {
 
-//     })
+        let identicalObjects = benz.isIdenticalTo(nanas)
+        assert.isTrue(identicalObjects)
+    })
+})
 
-//     it("should return false if nanas' keys don't match benz's keys", () => {
+describe("Test Character.update", () => {
 
-//         delete nanas['age']
-//         let identicalObjects = benz.isIdenticalTo(nanas)
-//         assert.isFalse(identicalObjects)
-//     })
+    it("adds a href attribute to the object", () => {
 
-// })
+        let serialised = benz.serialise('https', 'localhost', `/api/characters/${benz.dataValues.id}`)
+        assert.property(benz.dataValues, 'href')
+        assert.include(benz.dataValues.href, `/api/characters/${benz.dataValues.id}`)
 
-// describe("Test Character.update", () => {
-
-//     it("should update the instance's fields", () => {
-
-//         let zizou = {name: "Zinedine Zidane"}
-//         benz.update(zizou)
-//         assert.equal(benz.name, "Zinedine Zidane")
-//     })
-// })
-
+    })
+})
