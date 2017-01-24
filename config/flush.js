6ebe2635dd1800254af89d44cc1b2153b8ee9a77
MilkshakePanda <iam.superpeluche@gmail.com>
2017-01-24 22:15:09 +0100

    delete uncessary flush file

--- config/flush.js
+++
@@ -1,6 +0,0 @@
-// const app = require('../app')
-const db  = require('./index')
-
-db.connection.sync({force: true})
-    .then( () => console.log("DB FLUSHED") )
-    .catch( err => console.log("Something happened") )
