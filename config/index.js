760ae128fc5da2798e5c7a2cf193b153b268a37b
MilkshakePanda <iam.superpeluche@gmail.com>
2017-01-25 16:55:19 +0100

    remove config variable and directly destructure process.env

--- config/index.js
+++ config/index.js
@@ -1,14 +1,17 @@
 const Sequelize         = require('sequelize')
-let connection, config
+let connection
 
 try {
-    config = { DB_NAME, DB_USER, DB_HOST, DB_ENGINE, DB_PASSWORD } = process.env
-    connection    = new Sequelize(config.DB_NAME, config.DB_USER, config.DB_PASSWORD, {
-        host: config.DB_HOST,
-        dialect: config.DB_ENGINE,
+
+    let { DB_NAME, DB_USER, DB_HOST, DB_ENGINE, DB_PASSWORD } = process.env
+
+    connection    = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+        host: DB_HOST,
+        dialect: DB_ENGINE,
         logging: false,
-        define: { timestamps: false}
+        define: { timestamps: false }
     })
+
 }
 catch(error) {
 
